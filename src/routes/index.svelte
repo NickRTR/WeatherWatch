ee644c81b3532a741750b9a184eeab998c9d4ad4
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-01-17 17:45:19 +0100

    added new features like humidity and wind report

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,15 @@
 <script>
-    let loc = "Balingen";
+    let loc = "Berlin";
 
     let promise = getWeather();
 
     async function getWeather() {
-        const key = "1ac68e62a6ed46e28da135216221501";
+        const key = "bba81dedf0f34bda955161436221701";
         const res = await fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${loc}`);
        
         if (res.ok) {
             const result = await res.json();
-            console.table(result);
+            console.log(result);
   		    return result;
 		} else {
 			throw new Error(await res.json());
@@ -30,7 +30,10 @@
     {#await promise}
         <p>Lade Wetter ...</p>
     {:then data} 
-        <p>In {data.location.name} hat es im Moment {Math.floor(data.current.temp_c)}°C</p>
+        <p>In {data.location.name}, {data.location.country} it is currently {Math.floor(data.current.temp_c)}°C</p>
+        <p>The air humidity is around {data.current.humidity}%</p>
+        <p>Additionally the wind blows with {data.current.wind_kph}km/h, which leads to the temperature feeling like {data.current.feelslike_c}°C</p>
+        <img src={data.current.condition.icon} alt={data.current.condition.text}>
     {:catch error}
         <p style="color: red">Error: Ort nicht vorhanden.</p>
     {/await}
