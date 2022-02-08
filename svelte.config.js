3833609f4e981f948eed0d5e3228210607cc2fcc
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-02-08 20:55:51 +0100

    fixed tauri:package

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static";
+
+const dev = process.env.NODE_ENV == "development"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: true
+		}),
 
 		vite: {
+			compilerOptions: { dev },
             define: {
                 'process.env': process.env,
             },
