6b674580ae2040f86a453118d2eea4a08ba057e9
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-03-23 20:05:00 +0100

    .

--- src/routes/[search].json.js
+++ src/routes/[search].json.js
@@ -3,7 +3,7 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.VITE_API_KEY;
 
 export async function get({ params }) {
     const result = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${params.search}&days=3&aqi=yes`);
