238e320adc5dd0f81a9f862aac144389b3386122
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-03-23 16:48:39 +0100

    Try to fix API_KEY issue

--- src/routes/api/suggestion/[location].json.js
+++ src/routes/api/suggestion/[location].json.js
@@ -1,4 +1,8 @@
-import { API_KEY } from "$lib/api";
+export let API_KEY = import.meta.env.VITE_API_KEY;
+// if production, get KEY from Vercel
+if (process.env.NODE_ENV === 'production') {
+    API_KEY = process.env.API_KEY;
+}
 
 export async function get({ params }) {
     const result = await fetch(`https://api.weatherapi.com/v1/search.json?key=${API_KEY}&q=${params.location}`);
