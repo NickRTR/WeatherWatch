c4bc4ffefca3ec46301d85e7ae8a2c4a4d837251
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-03-23 15:24:32 +0100

    ...

--- src/routes/api/[search].json.js
+++ src/routes/api/[search].json.js
@@ -1,4 +1,8 @@
-import { API_KEY } from "$lib/api";
+export let API_KEY = import.meta.env.VITE_API_KEY;
+// if production, get KEY from Vercel
+if (process.env.NODE_ENV === 'production') {
+    API_KEY = process.env.API_KEY;
+}
 
 export async function get({ params }) {
     let result = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${params.search}&days=3&aqi=yes`);
