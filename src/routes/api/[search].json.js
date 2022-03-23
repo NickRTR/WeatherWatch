c4667bc1bd56a5ff03405c7a519353de9d459941
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-03-23 17:02:29 +0100

    Fix API_KEY

--- src/routes/api/[search].json.js
+++ src/routes/api/[search].json.js
@@ -1,11 +1,11 @@
-export let API_KEY = import.meta.env.VITE_API_KEY;
+let API_KEY = import.meta.env.VITE_API_KEY;
 // if production, get KEY from Vercel
 if (process.env.NODE_ENV === 'production') {
     API_KEY = process.env.API_KEY;
 }
 
 export async function get({ params }) {
-    let result = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${params.search}&days=3&aqi=yes`);
+    const result = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${params.search}&days=3&aqi=yes`);
     let data = await result.json();
 
     if (result.ok) {
